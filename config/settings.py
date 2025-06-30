253946624887512c15ecb555ad0a534446059108
Arsha1864 <qdirbaevaydos@gmail.com>
2025-06-30 21:30:27 +0900

    Add render config

--- config/settings.py
+++ config/settings.py
@@ -89,12 +89,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
+if os.getenv('DATABASE_URL'):
+  DATABASES = {
         'default':
            dj_database_url.config(default=os.getenv('DATABASE_URL',f'sqlite:///{BASE_DIR}/db.sqlite3'))
 }
-
+else: 
+  DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
