18bf166c1306d46b81f82ec15713ef59af70f6b8
Arsha1864 <qdirbaevaydos@gmail.com>
2025-07-05 15:52:29 +0900

    Add render config

--- card/urls.py
+++ card/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import CardCreateView,CardListView, VerifyCardSmsView, ResendCardSmsView
 
 urlpatterns = [
-    path('cards/add/', CardCreateView.as_view(), name='add-card'),
-    path('cards/', CardListView.as_view(), name='card-list'),
+    path('card/add/', CardCreateView.as_view(), name='add-card'),
+    path('card/', CardListView.as_view(), name='card-list'),
     path('verify_card_sms/', VerifyCardSmsView.as_view(), name='verify-card-sms'),
     path('resend_card_sms/', ResendCardSmsView.as_view(), name='resend-card-sms'),
 ]
