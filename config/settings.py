ceef760d94fb87da528aa4651a036773b9910db5
Arsha1864 <qdirbaevaydos@gmail.com>
2025-07-18 00:03:25 +0900

    Add render config

--- config/settings.py
+++ config/settings.py
@@ -165,9 +165,7 @@ INSTALLED_APPS = [
 ]
 AUTH_USER_MODEL = 'accounts.CustomUser'
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
+
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
